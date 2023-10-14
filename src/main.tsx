1ba77850ed4ef8fb0021a3aeec4963eeb18a8f3b
Vignesh Kumar <reactivicky@gmail.com>
2023-10-14 20:23:17 +0530

    added mui

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,13 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <CssBaseline />
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
